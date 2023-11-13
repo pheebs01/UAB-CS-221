971bd19790ff0777caeb94551211406e142d4ebc
pheebs01 <104721249+pheebs01@users.noreply.github.com>
2023-11-13 10:30:42 -0600

    3 files added

---
+++ final project/project.js
@@ -0,0 +1,88 @@
+// This is what I have so far.
+// This is a simple example to get you started. You can expand and improve upon this.
+
+const wordCategories = ['general', 'animals', 'sports']; // Add more categories as needed
+let currentWord = '';
+let guessedLetters = [];
+let hangmanFigureState = 0; // You can use this to track the hangman figure progression
+let guessCount = 0;
+let timer = 0;
+
+// Function to start a new game
+function startGame() {
+    // Reset game state
+    currentWord = getRandomWord();
+    guessedLetters = [];
+    hangmanFigureState = 0;
+    guessCount = 0;
+    timer = 0;
+
+    // Display initial UI
+    updateWordDisplay();
+    updateHangmanFigure();
+    updateGuessCount();
+    updateTimer();
+    generateLetterButtons();
+}
+
+// Function to get a random word from the selected category
+function getRandomWord() {
+    const category = wordCategories[Math.floor(Math.random() * wordCategories.length)];
+    // Logic to fetch a random word from the chosen category (you can use an API or a predefined list)
+    // For now, let's assume we have a predefined list for each category
+    const words = {
+        'general': ['example', 'hangman', 'project'],
+        'animals': ['elephant', 'tiger', 'giraffe'],
+        'sports': ['soccer', 'basketball', 'tennis']
+    };
+    return words[category][Math.floor(Math.random() * words[category].length)];
+}
+
+// Function to update the displayed word with guessed letters
+function updateWordDisplay() {
+    const wordDisplayElement = document.getElementById('word-display');
+    wordDisplayElement.textContent = currentWord
+        .split('')
+        .map(letter => (guessedLetters.includes(letter) ? letter : '_'))
+        .join(' ');
+}
+
+// Function to update the hangman figure based on incorrect guesses
+function updateHangmanFigure() {
+    const hangmanFigureElement = document.getElementById('hangman-figure');
+    // Logic to update the hangman figure based on hangmanFigureState
+    // (you can use CSS classes and switch classes to show different hangman figures)
+}
+
+// Function to update the guess count
+function updateGuessCount() {
+    document.getElementById('guess-count').textContent = guessCount;
+}
+
+// Function to update the timer
+function updateTimer() {
+    document.getElementById('timer').textContent = timer;
+}
+
+// Function to generate letter buttons dynamically
+function generateLetterButtons() {
+    const lettersElement = document.getElementById('letters');
+    lettersElement.innerHTML = '';
+    for (let i = 65; i <= 90; i++) {
+        const letter = String.fromCharCode(i);
+        const button = document.createElement('button');
+        button.textContent = letter;
+        button.addEventListener('click', () => handleLetterClick(letter));
+        lettersElement.appendChild(button);
+    }
+}
+
+// Function to handle letter button clicks
+function handleLetterClick(letter) {
+    // Logic to handle letter clicks
+    // Update guessedLetters, check if the letter is in the word, update game state, etc.
+    // You can add more logic for hint button, multiplayer mode, etc.
+}
+
+// Initialize the game
+startGame();
