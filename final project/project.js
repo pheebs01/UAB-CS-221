11f9f8508b3362c5afbf0c5e90f487f524285d53
pheebs01 <104721249+pheebs01@users.noreply.github.com>
2023-12-04 13:47:27 -0600

    1 file modified

--- final project/project.js
+++ final project/project.js
@@ -1,4 +1,23 @@
-import wordList from './words';
+const wordCategories = ['general', 'animals', 'sports'];
+
+const wordsAndHints = {
+    'general': {
+        hints: 'A broad category',
+        words: ['example', 'hangman', 'project', 'flower', 'computer', 'science', 'final']
+    },
+    'animals': {
+        hints: 'A living organism that typically can move and can feed on organic substances',
+        words: ['elephant', 'tiger', 'giraffe', 'lion', 'bear', 'monkey']
+    },
+    'sports': {
+        hints: 'Physical activities that involve skill and competition',
+        words: ['soccer', 'basketball', 'tennis', 'football', 'gymnastics']
+    },
+    'fashion': {
+        hints: 'Clothing: design, style, type, or article; or pop culture',
+        words: ['sweater', 'argyle', 'pants', 'plaid', 'skirt', 'vogue']
+    }
+};
 
 let currentWord = '';
 let guessedLetters = [];
@@ -15,7 +34,7 @@ function startGame() {
     hangmanFigureState = 0;
     guessCount = 0;
     timer = 0;
-  
+
     // Display initial UI
     updateWordDisplay();
     updateHangmanFigure();
@@ -26,7 +45,7 @@ function startGame() {
     displayMessage("reset");
     clearInterval(timerInterval);
     setTimer();
-  }
+}
 
 function setTimer() {
     // Start the timer interval
@@ -51,18 +70,28 @@ function endGame() {
 
 // Function to get a random word from the selected category
 function getRandomWord() {
-    const randomIndex = Math.floor(Math.random() * wordList.length);
-    const randomWord = wordList[randomIndex];
-  
-    console.log('Selected word:', randomWord.word);
-    console.log('Hint:', randomWord.hint);
-  
-    return randomWord.word;
-  }
-  
-  let correctLetters = [];
-  let incorrectLetters = [];
-  let displayedWord = '';
+    const category = wordCategories[Math.floor(Math.random() * wordCategories.length)];
+    console.log('Selected category:', category);
+
+    const selectedWords = wordsAndHints[category];
+    console.log('Words for the category:', selectedWords);
+
+    // Check if there are words in the selected category
+    if (selectedWords && selectedWords.words.length > 0) {
+        // Return a random word from the chosen category
+        const randomIndex = Math.floor(Math.random() * selectedWords.words.length);
+        const randomWord = selectedWords.words[randomIndex];
+        console.log('Selected word:', randomWord);
+        return randomWord;
+    } else {
+        console.error('No words found for the selected category.');
+        return ''; // Return an empty string if no words are found
+    }
+}
+
+let correctLetters = [];
+let incorrectLetters = [];
+let displayedWord = '';
 
 // Function to update the displayed word with guessed letters
 function updateWordDisplay() {
@@ -96,10 +125,6 @@ function resetIncorrectLetters() {
     incorrectLettersElement.textContent = ``;
     incorrectLetters = [];
 }
-
-// Define hangmanParts outside of handleLetterClick
-const hangmanParts = ['head', 'body', 'left-arm', 'right-arm', 'left-leg', 'right-leg'];
-
 
 // Function to update the hangman figure based on incorrect guesses
 function updateHangmanFigure() {
@@ -150,23 +175,27 @@ function generateLetterButtons() {
     }
 }
 
+
+// Define hangmanParts outside of handleLetterClick
+const hangmanParts = ['head', 'body', 'left-arm', 'right-arm', 'left-leg', 'right-leg'];
+
 // Function to display messages on the main screen
 function displayMessage(message) {
     const messageArea = document.getElementById('message-area');
     // Allows the reset button to remove the display area
-    if (message == "reset") {
+     if(message == "reset") {
         messageArea.style.visibility = "hidden";
-    } else {
-        messageArea.style.visibility = "visible";
+    } else { 
+        messageArea.style.visibility = "visible"; 
         messageArea.textContent = message;
         messageArea.classList.add('show');
     }
 }
 
-setTimeout(() => {
-    const messageArea = document.getElementById('message-area');
-    messageArea.classList.remove('show');
-}, 3000);
+    setTimeout(() => {
+        const messageArea = document.getElementById('message-area');
+        messageArea.classList.remove('show');
+    }, 3000);
 
 // Function to handle letter button clicks
 function handleLetterClick(letter) {
@@ -199,7 +228,7 @@ function handleLetterClick(letter) {
             // Check if the game has been won or lost after the update
             if (hangmanFigureState === hangmanParts.length) {
                 // The hangman figure is complete
-                displayMessage(`Game over - Hangman figure complete! The word was: ${currentWord}`);
+                displayMessage('Game over - Hangman figure complete!');
                 endGame();
             } else if (displayedWord.toLowerCase() === currentWord.toLowerCase()) {
                 // All letters have been guessed
@@ -236,24 +265,6 @@ function getHint() {
     } else {
         displayMessage('No hint available for this word.');
     }
-}
-
-var isLight = true;
-
-//Function to toggle Lightmode / Darkmode.
-function darkToggle() {
-    var title = document.getElementById("title");
-    title.classList.toggle("titledark-mode");
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    var darkButton = document.getElementById("darkButton");
-    if (isLight) {
-        darkButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="fill:goldenrod;" height="45" viewBox="0 -960 960 960" width="45"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" /></svg>`;
-    } else {
-        darkButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="fill:goldenrod;" height="45" viewBox=" 0 -960 960 960" width="45">
-        <path d = "M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" /></svg >`;
-    }
-    isLight = !isLight;
 }
 
 // Initialize the game
